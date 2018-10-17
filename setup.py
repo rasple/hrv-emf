c78f91ce56511a36819039db2c0906a16461d904
rasple <frankme97@gmail.com>
2018-10-17 09:20:50 +0200

    Changes to setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(name='hrv-emf-server',
+      version='0.1',
+      description='Server for HRV measurements',
+      url='https://github.com/rasple/hrv-emf-server',
+      author='Frank Meier',
+      author_email='it16078@lehre.dhbw-stuttgart.de',
+      license='GPLv3',
+      packages=find_packages(),
+      python_requires=">=3.5",
+      install_requires=[]
+      )
+
